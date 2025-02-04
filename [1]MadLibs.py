470237883e062abe69bd460076b2a31958d53783
ChavvaAnitha123 <chavvaanithareddy18@gmail.com>
2025-02-04 10:06:21 +0530

    Python-file

---
+++ [1]MadLibs.py
@@ -0,0 +1,8 @@
+#Madlibs
+
+animal = input("Animal: ")
+adjective = input("Adjectives: ")
+verb = input("Verb: ")
+
+madlib = f"Once, there was a {animal} who was very {adjective}. It loved to {verb} all day long!"
+print(madlib)
